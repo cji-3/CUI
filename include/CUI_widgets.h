03eb4055931e3c0c8418de6b4aa65af47f8e8c3e
cji-3 <316007@st.csvs.chc.edu.tw>
2026-05-03 07:32:26 +0800

    我也不知道

--- include/CUI_widgets.h
+++ include/CUI_widgets.h
@@ -9,6 +9,8 @@
 #ifndef _CUI_widgets_H_
 #define _CUI_widgets_H_
 
+#include<stdbool.h>
+
 //#include "CUI_def.h"
 
 //設定C函數定義，使使用C++時也是如此
@@ -16,7 +18,17 @@
 extern "C" {
 #endif
 
-
+typedef strcat{
+	int x;					/**< x座標 */
+	int y;					/**< y座標 */
+	int w;					/**< 寬 */
+	int h;					/**< 高 */
+	const char* text;		/**< 文字 */
+	SDL_Color prospect;		/**< 前景色 */
+	SDL_Color background;	/**< 背景色 */
+	const char* image;		/**< 圖片路徑 */
+	bool shown;				/**< 是否顯示 */
+}CUI_Display;
 
 //設定C函數定義，使使用C++時也是如此
 #ifdef __cplusplus
