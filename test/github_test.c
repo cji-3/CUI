ac7c37a297d12341815296d6c5e341e4ffbfe9b7
cji-3 <316007@st.csvs.chc.edu.tw>
2026-05-03 19:35:46 +0800

    test.c也不該引入SDL

--- test/github_test.c
+++ test/github_test.c
@@ -1,15 +1,14 @@
-#include "../include/CUI.h"
-#include "../extern/SDL/include/SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../include/CUI.h"
 int main(int argc, char* argv[]) {
    //內容測試放置區域
     CUI_Init("test",50,50,640,360,CUI_INIT_WINDOW_MODE_SHOWN);
     if (CUI_GetWindowWidth()!=640 || CUI_GetWindowHeight()!=360) {
         printf("窗口尺寸不正確！\n");
-        return 1; 
+        return 1;
     }
     printf("窗口尺寸正確！\n");
-    return 0; 
+    return 0;
 }
--- test/test.c
+++ test/test.c
@@ -1,22 +1,12 @@
-#include "../include/CUI.h"
-#include "../extern/SDL/include/SDL.h"
 #include<stdbool.h>
+#include<windows.h>
+
+#include "../include/CUI.h"
 
 int main(int argc,char* argv[]) {
 	CUI_Init("test",50,50,640,360,CUI_INIT_WINDOW_MODE_SHOWN);
 
-	SDL_Event event;
-    bool running = true;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) { //按x退出
-                running = false;
-            }
-        }
-
-        // 可以在這裡呼叫未來的 CUI_Render()
-        SDL_Delay(16); // 簡單限制一下 CPU 使用率 (約 60 FPS)
-    }
+    Sleep(1000);
 
 	CUI_Quit();
 	return 0;
